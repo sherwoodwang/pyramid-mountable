cd1ca0c68ec7e9e38f0f3bb9949376b60db7e323
Sherwood Wang | 王嘯宇 <swang.work@gmail.com>
2015-05-10 20:45:04 +0800

    init

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import os
+
+from setuptools import setup, find_packages
+
+setup(name='pyramid_moutable',
+      version='0.1',
+      description='Provide a mountable tree to the ease creation of traversal root for pyramid',
+      classifiers=[
+          "Programming Language :: Python",
+          "Framework :: Pyramid",
+          "Topic :: Internet :: WWW/HTTP",
+      ],
+      author='Sherwood Wang',
+      author_email='sherwood@wang.onl',
+      packages=find_packages(),
+      install_requires=[
+          'pyramid',
+          'venusian',
+          'zope.interface',
+          'zope.proxy',
+      ]
+      )
